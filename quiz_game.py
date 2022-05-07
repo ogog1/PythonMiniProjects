63f27e81a046a7bbc682fc67a18cc3c660c609f2
ogog1 <gorkeyomer@gmail.com>
2022-05-07 15:50:56 +0300

    new

---
+++ quiz_game.py
@@ -0,0 +1,12 @@
+print("Hi! Welcome to my quiz!")
+
+playing = input("Want to take the quiz? (Y/N) ")
+
+if "y" in playing[0] or "Y" in playing[0]:
+    print("Let's play")
+else:
+    print("Maybe later...")
+    quit()
+
+
+
