e058caa2d6f1d2eb23049dc1070d269b690c1b81
ogog1 <gorkeyomer@gmail.com>
2022-05-07 16:24:57 +0300

    new

--- quiz_game.py
+++ quiz_game.py
@@ -10,10 +10,16 @@ else:
 
 score = 0
 
+
 Question1 = "What element does O represent on the periodic table? "
 answer1 = input(Question1)
 
-if answer1 == "oxygen" or answer1 == "Oxygen" or answer1 == "OXYGEN":
-    score = score + 1
-    print(f"That's correct! Your score is {score}/10." )
+    if answer1 == "oxygen" or answer1 == "Oxygen" or answer1 == "OXYGEN":
+        score = score + 1
+        print(f"That's correct! Your score is {score}/10.")
+    else:
+        print(f"Sorry,that's not correct... Your score is {score}/10.")
+        again = input("Want to try again?")
+        if "n" in again[0] or "N" in again[0]:
+
 
