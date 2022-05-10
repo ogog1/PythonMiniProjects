73c17202a92a2fc6007825662a1b227a704ef0ac
ogog1 <gorkeyomer@gmail.com>
2022-05-10 21:04:14 +0300

    new

--- quiz_game.py
+++ quiz_game.py
@@ -42,3 +42,20 @@ while True:
         else:
             print("Next question!")
             break
+
+while True:
+    Question3 = "What's the capital of Spain? "
+    answer3 = input(Question3)
+
+    if answer3 == "madrid" or answer3 == "Madrid" or answer3 == "MADRID":
+        score = score + 1
+        print(f"That's correct! Your score is {score}/10.")
+        break
+    else:
+        print(f"Sorry,that's not correct... Your score is {score}/10.")
+        again = input("Want to try again? ")
+        if "y" in again[0] or "Y" in again[0]:
+            True
+        else:
+            print("Next question!")
+            break
